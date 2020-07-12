dd0ee33c06b100be7e706b2e98db4e7aaea47e94
610555061 <m18301398862@163.com>
2020-07-12 22:15:16 +0800

    review path of pics
    review path of pics

--- chapter_12/ship.py
+++ chapter_12/ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship():
 
     def __init__(self, ai_settings, screen):
@@ -8,21 +9,21 @@ class Ship():
         self.ai_settings = ai_settings
 
         # Load the ship image, and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load(r'C:\Users\Jiaxi\Documents\GitHub\pcc\chapter_12\images\ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        
+
         # Store a decimal value for the ship's center.
         self.center = float(self.rect.centerx)
-        
+
         # Movement flags.
         self.moving_right = False
         self.moving_left = False
-        
+
     def update(self):
         """Update the ship's position, based on movement flags."""
         # Update the ship's center value, not the rect.
@@ -30,7 +31,7 @@ class Ship():
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-            
+
         # Update rect object from self.center.
         self.rect.centerx = self.center
 
